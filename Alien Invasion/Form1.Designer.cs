e5cd9b2bd409695112ac252a602ff15af958032b
Roberto Stevkovski <stevkovskiroberto@gmail.com>
2021-08-29 20:37:38 +0200

    Init

---
+++ Alien Invasion/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Alien_Invasion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.txtScore = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pillar2 = new System.Windows.Forms.PictureBox();
+            this.pillar1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(13, 13);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(118, 26);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "SCORE: 0";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevel.ForeColor = System.Drawing.Color.White;
+            this.lblLevel.Location = new System.Drawing.Point(801, 13);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(112, 26);
+            this.lblLevel.TabIndex = 5;
+            this.lblLevel.Text = "LEVEL: 1";
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lblGameOver.Location = new System.Drawing.Point(277, 192);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(391, 70);
+            this.lblGameOver.TabIndex = 6;
+            this.lblGameOver.Text = "         GAME OVER! \r\nPRESS ENTER TO RETRY";
+            this.lblGameOver.Visible = false;
+            // 
+            // ufo
+            // 
+            this.ufo.Image = global::Alien_Invasion.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(820, 283);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(66, 40);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ufo.TabIndex = 3;
+            this.ufo.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::Alien_Invasion.Properties.Resources.Spaceship1;
+            this.player.Location = new System.Drawing.Point(101, 154);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(133, 35);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // pillar2
+            // 
+            this.pillar2.Image = global::Alien_Invasion.Properties.Resources.pillar5;
+            this.pillar2.Location = new System.Drawing.Point(244, 304);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(66, 174);
+            this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar2.TabIndex = 1;
+            this.pillar2.TabStop = false;
+            this.pillar2.Tag = "pillar";
+            // 
+            // pillar1
+            // 
+            this.pillar1.BackColor = System.Drawing.Color.Transparent;
+            this.pillar1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pillar1.Image = global::Alien_Invasion.Properties.Resources.pillar5;
+            this.pillar1.Location = new System.Drawing.Point(576, -1);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(66, 190);
+            this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar1.TabIndex = 0;
+            this.pillar1.TabStop = false;
+            this.pillar1.Tag = "pillar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(925, 478);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pillar2);
+            this.Controls.Add(this.pillar1);
+            this.Name = "Form1";
+            this.Text = "Alien Invasion";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pillar1;
+        private System.Windows.Forms.PictureBox pillar2;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label lblGameOver;
+    }
+}
+
---
+++ Alien Invasion/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Alien_Invasion
+{
+    public partial class Form1 : Form
+    {
+        bool goUp, goDown, shot, gameOver;
+        int score = 0;
+        int speed = 8;
+        int UFOspeed = 10;
+
+        Random rand = new Random();
+
+        int playerSpeed = 7;
+        int index = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            lblGameOver.Visible = false;
+            txtScore.Text = "SCORE: " + score;
+            lblLevel.ForeColor = Color.White;
+
+            if (goUp == true && player.Top > 0)
+            {
+                player.Top -= playerSpeed;
+            }
+            if (goDown == true && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += playerSpeed;
+            }
+
+            ufo.Left -= UFOspeed;
+
+            if(ufo.Left + ufo.Width < 0)
+            {
+                ChangeUFO();
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "pillar")
+                {
+                    x.Left -= speed;
+
+
+                    if (x.Left < -200)
+                    {
+                        x.Left = 1000;
+                    }
+
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        GameOver();
+                    }
+
+                }
+
+                if (x is PictureBox && (string)x.Tag == "bullet")
+                {
+                    x.Left += 25;
+
+                    if (x.Left > 900)
+                    {
+                        RemoveBullet(((PictureBox)x));
+                    }
+
+
+                    if (ufo.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        RemoveBullet(((PictureBox)x));
+                        score += 1;
+                        ChangeUFO();
+                    }
+
+                    if (x.Bounds.IntersectsWith(pillar1.Bounds))
+                    {
+                        RemoveBullet(((PictureBox)x));
+                    }
+                    if (x.Bounds.IntersectsWith(pillar2.Bounds))
+                    {
+                        RemoveBullet(((PictureBox)x));
+                    }
+
+                }
+
+            }
+
+
+            if (player.Bounds.IntersectsWith(ufo.Bounds))
+            {
+                GameOver();
+            }
+
+            if (score > 4)
+            {
+                speed = 12;
+                UFOspeed = 18;
+                lblLevel.Text = "LEVEL: 2";
+                lblLevel.ForeColor = Color.Yellow;
+            }
+            if (score > 8 )
+            {
+                speed = 15;
+                UFOspeed = 20;
+                lblLevel.Text = "LEVEL: 3";
+                lblLevel.ForeColor = Color.Red;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+
+            if (e.KeyCode == Keys.Space && shot == false)
+            {
+                MakeBullet();
+                shot = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (shot == true)
+            {
+                shot = false;
+            }
+
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+        }
+
+        private void RestartGame()
+        {
+            goUp = false;
+            goDown = false;
+            shot = false;
+            gameOver = false;
+            score = 0;
+            speed = 8;
+            UFOspeed = 10;
+
+
+            txtScore.Text = "SCORE: " + score;
+            lblLevel.Text = "LEVEL: 1";
+
+            ChangeUFO();
+
+            player.Top = 119;
+            pillar1.Left = 617;
+            pillar2.Left = 244;
+
+            GameTimer.Start();
+        }
+
+        private void GameOver()
+        {
+            GameTimer.Stop();
+            txtScore.Text = "SCORE: " + score;
+            lblGameOver.Visible = true;
+            gameOver = true;
+        }
+
+        
+
+        private void RemoveBullet(PictureBox bullet)
+        {
+            this.Controls.Remove(bullet);
+            bullet.Dispose();
+        }
+
+        private void MakeBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.BackColor = Color.Yellow;
+            bullet.Height = 5;
+            bullet.Width = 10;
+
+            bullet.Left = player.Left + player.Width;
+            bullet.Top = player.Top + player.Height / 2;
+
+            bullet.Tag = "bullet";
+
+            this.Controls.Add(bullet);
+        }
+
+        private void ChangeUFO()
+        {
+            if(index > 3)
+            {
+                index = 1;
+
+            }
+            else
+            {
+                index += 1;
+            }
+
+            switch (index)
+            {
+                case 1:
+                    ufo.Image = Properties.Resources.alien1;
+                    break;
+                case 2:
+                    ufo.Image = Properties.Resources.alien2;
+                    break;
+                case 3:
+                    ufo.Image = Properties.Resources.alien3;
+                    break;
+            }
+            ufo.Left = 1000;
+            ufo.Top = rand.Next(20, this.ClientSize.Height - ufo.Height);
+        }
+    }
+}
